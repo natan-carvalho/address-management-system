eaa93da09e7be0d302d6170491944d496cc2aced
natan <natan.carvalho123@hotmail.com>
2024-05-29 10:04:14 -0300

    :heavy_plus_sign: build: Created the User and address models and added the package and its EntityFramework dependencies

---
+++ AeCAddress/Models/AddressModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AeCAddress;
+
+public class AddressModel
+{
+  public int Id { get; set; }
+  public required string CEP { get; set; }
+  public required string Logradouro { get; set; }
+  public string? Complement { get; set; }
+  public required string Bairro { get; set; }
+  public required string Cidade { get; set; }
+  public required string UF { get; set; }
+  public required string Numero { get; set; }
+  public required int UsuarioID { get; set; }
+
+}
---
+++ AeCAddress/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AeCAddress;
+
+public class UserModel
+{
+  public int Id { get; set; }
+  public required string Nome { get; set; }
+  public required string Usuario { get; set; }
+  public required string Senha { get; set; }
+}
